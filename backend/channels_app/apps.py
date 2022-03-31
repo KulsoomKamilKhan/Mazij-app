0c07a4689f741f88d908936c2cf32e71c887984d
Bhavika <71709655+bhavikakaliya@users.noreply.github.com>
2022-03-31 12:32:51 +0400

    Delete backend directory

--- backend/channels_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChannelsAppConfig(AppConfig):
-    name = 'channels_app'
--- backend/posts/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Post, Draft 
-admin.site.register(Post)
-admin.site.register(Draft)
